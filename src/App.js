df05770358f3ff8f76b4dd232a30851153afaef5
Mulew <muluken1393@gmail.com>
2023-08-11 19:11:27 +0300

    all are new files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { faBars } from '@fortawesome/free-solid-svg-icons';
+//import '@fortawesome/fontawesome-free/css/all.css';
+import Calculator from './Calculator/calculator';
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+ render(){
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+ }
+}
+
---
+++ src/Calculator/calculator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './calculator.css';
+export default class Calculator extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className='Container'>
+                <div className='screen'>
+                    <h1>Screen Area</h1>
+                </div>
+                <div className='numbers'>
+                    <h4 className='item1'>AC</h4>
+                    <h4 className='item1'>+/-</h4>
+                    <h4 className='item1'>%</h4>
+                    <h4 className='item0'>+</h4>
+                    <h4 className='item1'>7</h4>
+                    <h4 className='item1'>8</h4>
+                    <h4 className='item1'>9</h4>
+                    <h4 className='item0'>*</h4>
+                    <h4 className='item1'>4</h4>
+                    <h4 className='item1'>5</h4>
+                    <h4 className='item1'>6</h4>
+                    <h4 className='item0'>-</h4>
+                    <h4 className='item1'>1</h4>
+                    <h4 className='item1'>2</h4>
+                    <h4 className='item1'>3</h4>
+                    <h4 className='item0'>+</h4>
+                    <h4 className='item1x'>0</h4>
+                    <h4 className='item0'>=</h4>
+                </div>
+            </div>
+        )
+    }
+}
